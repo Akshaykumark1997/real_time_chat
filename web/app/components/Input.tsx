e0dbd923fb5494452b1b17b304239607f3044cbd
Akshay kumar k <akshayajith40@gmail.com>
2025-07-01 12:53:36 +0530

    feat: Enhance login page layout and functionality. Update the design with a welcoming message and image, and add an input component for user email entry. Introduce a new image asset for improved visual appeal.

---
+++ web/app/components/Input.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Input() {
+  return (
+    <div>
+      <input type="text" placeholder="Enter your email" />
+    </div>
+  );
+}
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,12 +1,38 @@
 'use client';
+import Image from "next/image";
+import Input from "./components/Input";
 
 
 export default function LoginPage() {
 
 
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-[#F0F4FC]">
-      <h1 className="text-4xl font-bold text-center text-red-500">Hello World</h1>
+    <div className="h-screen w-screen flex items-center justify-between bg-[#F0F4FC]">
+      <div className="w-1/2 h-full flex flex-col items-start p-32 justify-center gap-2">
+        <h1 className="text-2xl text-black">Welcome to</h1>
+        <h1 className="text-4xl text-black">Your Platform Name</h1>
+        <p className="text-black text-xs">
+          Here, we believe that building a strong professional network begins
+          with your participation. We are delighted to offer a modern and
+          user-friendly service to ensure you have the best experience.
+        </p>
+        <p className="text-[#4461F2] text-base">Join Now!</p>
+        <Image
+          src={"/images/Frame.png"}
+          alt="frame_image"
+          width={558}
+          height={370}
+        />
+      </div>
+      <div className="w-1/2 h-full flex flex-col gap-4">
+        <div className="flex gap-4">
+          <h3 className="text-black text-2xl">Sign In</h3>
+          <h3 className="text-black text-2xl">Register</h3>
+        </div>
+        <div>
+          <Input />
+        </div>
+      </div>
     </div>
   );
 }
