435ace5f597fcb83b31f0b9c4b607a75ded030be
Akshay kumar k <akshayajith40@gmail.com>
2025-07-01 12:11:55 +0530

    init: Initialize Next.js project with TypeScript, ESLint, and Tailwind CSS setup. Add essential configuration files, including .gitignore, package.json, and TypeScript configuration. Create initial application structure with global styles, layout, and a sample login page.

---
+++ web/app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+
+export default function LoginPage() {
+
+
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-[#F0F4FC]">
+      <h1 className="text-4xl font-bold text-center text-red-500">Hello World</h1>
+    </div>
+  );
+}
